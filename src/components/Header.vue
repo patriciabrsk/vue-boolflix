08b9ca0e64ca77d34b4275ba1c89e60849b22305
patriciabrsk <patriciabresku@gmail.com>
2022-04-01 12:17:51 +0200

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header"></div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Header",
+  props: {},
+};
+</script>
+
+<style scoped lang="scss"></style>
