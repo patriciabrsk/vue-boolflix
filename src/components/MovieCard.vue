978dad7f1a98c93073136b6a2d68c26b6a27e417
patriciabrsk <patriciabresku@gmail.com>
2022-04-05 19:39:53 +0200

    card click

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="justify-content-center d-flex">
     <div class="col">
-      <div class="movie-card h-100 rounded-3">
-        <div class="front-image">
-          <img
-            class="img-fluid rounded-3"
-            :src="`https://image.tmdb.org/t/p/w342/${movieData.poster_path}`"
-            :alt="movieData.title"
-          />
-        </div>
-        <div class="back-info d-none">
-          <h2 class="text-white title">
-            Titolo: {{ movieData.title || movieData.name }}
-          </h2>
-          <p class="text-white original-title">
-            Titolo originale:
-            {{ movieData.original_title || movieData.original_name }}
-          </p>
-          <p class="overview">{{ movieData.overview }}</p>
-          <country-flag :country="movieData.original_language" size="small" />
-          <p></p>
+      <div
+        class="movie-card rounded-3 p-0"
+        @click="flipCard()"
+        :class="{ flipped: isClicked }"
+      >
+        <div class="flip-card-wrapper">
+          <div class="front-image">
+            <img
+              class="rounded"
+              :src="`https://image.tmdb.org/t/p/w342/${movieData.poster_path}`"
+              :alt="movieData.title"
+            />
+          </div>
+          <div class="back-info d-none overflow-auto">
+            <h2 class="title text-white">
+              Titolo: {{ movieData.title || movieData.name }}
+            </h2>
+            <p class="original-title text-white">
+              Titolo originale:
+              {{ movieData.original_title || movieData.original_name }}
+            </p>
+            <p class="overview">{{ movieData.overview }}</p>
+            <country-flag :country="movieData.original_language" size="small" />
+            <p></p>
+          </div>
         </div>
       </div>
     </div>
@@ -34,22 +40,58 @@ export default {
   },
   data() {
     return {
-      hover: false,
+      isClicked: false,
     };
+  },
+  methods: {
+    flipCard() {
+      this.isClicked = !this.isClicked;
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "../assets/style/style.scss";
-.active {
-}
+
 div.movie-card {
-  border: 2px solid transparent;
+  -webkit-transition: -webkit-transform 0.6s;
+  -moz-transition: -moz-transform 0.6s;
+  -o-transition: -o-transform 0.6s;
+  transition: transform 0.6s;
+  -webkit-transform-style: preserve-3d;
+  -moz-transform-style: preserve-3d;
+  -o-transform-style: preserve-3d;
+  transform-style: preserve-3d;
+  position: relative;
+
   &:hover {
-    border: 2px solid red;
     transform: scale(1.01);
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.6);
     cursor: pointer;
+  }
+
+  .movie-card.flipped {
+    -webkit-transform: rotateY(180deg);
+    transform: rotateY(180deg);
+  }
+
+  .movie-card .front-image,
+  .movie-card .back-info {
+    color: white;
+    position: absolute;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    box-shadow: 3px 5px 20px 2px rgba(0, 0, 0, 0.25);
+    -webkit-box-shadow: 0 2px 10px rgba(0, 0, 0, 0.16),
+      0 2px 5px rgba(0, 0, 0, 0.26);
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.16), 0 2px 5px rgba(0, 0, 0, 0.26);
+  }
+
+  div.front-image img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
 }
 </style>
