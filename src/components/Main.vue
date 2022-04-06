b958e69c75184db666b068f9f9ee181084d160b8
patriciabrsk <patriciabresku@gmail.com>
2022-04-06 13:27:51 +0200

    fix col in lg screen sizes

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,21 +1,30 @@
 <template>
   <div class="container-fluid p-4 mb-4">
-    <div class="row row-cols-4 g-4" v-if="moviesList.length > 0">
+    <div
+      class="row row-cols-4 row-cols-md-5 row-cols-lg-6 g-4"
+      v-if="moviesList.length > 0"
+    >
       <MovieCard
         :movieData="movie"
         v-for="(movie, index) in moviesList"
         :key="index"
       />
     </div>
-    <div class="row row-cols-4 g-4" v-if="seriesList.length > 0">
+    <div
+      class="row row-cols-4 row-cols-md-5 row-cols-lg-6 g-4"
+      v-if="seriesList.length > 0"
+    >
       <MovieCard
         :movieData="show"
         v-for="(show, index) in seriesList"
         :key="index"
       />
     </div>
-    <div class="row row-cols-4 g-4" v-if="popularList.length > 0">
-      <div class="col-12 text-white mb-0">
+    <div
+      class="row row-cols-4 row-cols-md-5 row-cols-lg-6 g-4"
+      v-if="popularList.length > 0"
+    >
+      <div class="col-12 col-12-lg text-white mb-0">
         <span class="badge text-uppercase fs-6 px-3 py-2 rounded-pill bg-dark">
           Most Popular
         </span>
