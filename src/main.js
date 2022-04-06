2a0ab30c49b0d96d9e08da7f75ef2773ae92953c
patriciabrsk <patriciabresku@gmail.com>
2022-04-06 10:17:34 +0200

    install npm fontawesome

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import CountryFlag from "vue-country-flag";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHatWizard } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faHatWizard);
 
 Vue.component("country-flag", CountryFlag);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 new Vue({
